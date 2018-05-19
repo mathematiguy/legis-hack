64b65a170bc826e7c015aebabc6800631d45428f
3Daniel <daniel@dan3d.org>
2018-05-19 22:15:26 +1200

    Added download capability

--- xmldownload.py
+++ xmldownload.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen, urlretrieve
 from bs4 import BeautifulSoup
 import re
+import os
 
 def read_url(url):
     url = url.replace(" ","%20")
@@ -25,11 +26,13 @@ def read_url(url):
         if match:
             #print("Group 1: " + match.group(1))
             year = int(match.group(1))
-            if year != None and year > 2000:
+            if year != None and year > 2017:
                 #print('Going in to dir ' + dirUrl)
                 
                 #Recurse in to the directory
                 read_url(dirUrl)
+                
+    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
     
     #Find any files to download
     files = (soup.find_all('li', class_='file'))
@@ -44,6 +47,24 @@ def read_url(url):
         if match:
             #download the file here...
             print("Download file: " + fileUrl)
+            req = Request(fileUrl)
+            xmlFile = urlopen(req).read()
+
+            #set up the directory to save the file
+            #this will be relative to the location of the python script!
+            file_path = os.path.join(script_dir, link['href'])
+            relative_file_path = "." + file_path
+            
+            #If the directory doesn't exist yet, create it recursively
+            dirname = os.path.dirname(relative_file_path)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            
+            #Open the new file and write the xml as a utf8 string
+            file = open(relative_file_path, 'w')
+            file.write(xmlFile.decode("utf-8"))
+            file.close()
+            print("Downloaded to: " + relative_file_path)
         
 
 read_url("http://legislation.govt.nz/subscribe/act/public")
