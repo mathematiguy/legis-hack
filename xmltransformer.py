9247cfd42b1c007de10c3c2303c8a9d2a6a11b60
3Daniel <daniel@dan3d.org>
2018-05-19 23:57:06 +1200

    Initial version of xmltransformer

---
+++ xmltransformer.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import re
+from bs4 import BeautifulSoup
+
+def xml_transform_asciidoc(file_path):
+    #setup the name of the output file
+    output_name = os.path.basename(file_path)[:-4] + "_output.xml"
+    output_file_path = os.path.join(os.path.dirname(file_path), output_name)
+    
+    print("Generating output: " + output_file_path)
+    
+    #creae the output file and open it
+    with open(file_path) as inputFile:
+        #parse the xml and get only the body tag
+        soup = BeautifulSoup(inputFile, "lxml-xml")
+        body = soup.find('body')
+        
+        #save the output file
+        with open(output_file_path, "w") as outputFile:
+            outputFile.write(body.decode())
+        
+#walks through the directory the script is located, passing matching files to the transform function
+def walk_dir():
+    walk_dir = os.path.dirname(os.path.realpath(__file__))
+    #print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+
+    for root, subdirs, files in os.walk(walk_dir):
+        for filename in files:
+            #regex to check if the file is original downloaded xml
+            pattern = re.compile("[a-f0-9]{16}\.xml")
+            match = pattern.search(filename)
+            if match:
+                file_path = os.path.join(root, filename)
+                xml_transform_asciidoc(file_path)
+                    
+walk_dir()
+
