3515132fcd4e43e3a606d71a03af4f7ed1159097
Dragonfly Administrator <cmos032@aucklanduni.ac.nz>
2018-05-22 22:31:21 +1200

    Implemented scrapy to replace xmldownload.py

--- legis/legis/spiders/legislation.py
+++ legis/legis/spiders/legislation.py
@@ -5,7 +5,7 @@ from urllib import urlopen
 
 class LegislationSpider(scrapy.Spider):
     
-    def __init__(self, from_year = 1993, to_year = 1993, **kwargs):
+    def __init__(self, from_year, to_year, **kwargs):
         self.from_year = from_year
         self.to_year = to_year
         super(LegislationSpider, self).__init__(**kwargs)
@@ -13,11 +13,12 @@ class LegislationSpider(scrapy.Spider):
     name = 'legislation'
     allowed_domains = ['legislation.govt.nz']
     start_urls = ['http://legislation.govt.nz/subscribe/act/public/']
-    root_dir = "data"
+    root_dir = "../legislation/xml"
 
     def parse(self, response):
         # get the li tags from the current page
         li_tags = response.xpath("/html/body/ul/li")
+        file_regex = re.compile("[^/]+$")
         for li in li_tags:
             # check the class of the li tag
             li_class = li.xpath("@class").extract()[0]
@@ -28,28 +29,25 @@ class LegislationSpider(scrapy.Spider):
             if year < self.from_year or year > self.to_year:
                 continue
 
-            file_regex   = re.compile("/subscribe/act/public/(.*)")
-            folder_regex = re.compile("/subscribe/act/public/(.*)/[^/]+$")
             if li_class == "directory":
                 # the tag points to a directory, so get the href 
                 next_page = response.urljoin(href)
                 # get folder_path from href
-                folder_path = os.path.join(
-                    self.root_dir, 
-                    file_regex.search(href).group(1))
+                folder_path = os.path.join(self.root_dir, href[1:])
+
                 if not os.path.exists(folder_path):
                     # create folder_path if it doesn't exist
                     self.logger.info("creating path %s" % folder_path)
                     os.makedirs(folder_path)
+
                 # go to the next page
                 yield scrapy.Request(next_page, callback = self.parse)
 
             elif li_class == "file" and href.endswith(".xml"):
                 file_url = response.urljoin(href)
-                file_path = os.path.join(
-                    self.root_dir, 
-                    file_regex.search(href).group(1))
-                if not os.path.exists(file_path):
+                file_path = os.path.join(self.root_dir, href[1:])
+                
+                if not os.path.isfile(file_path):
                     self.logger.info("reading file %s" % file_path)
                     f = urlopen(file_url)
                     f_text = f.read()
@@ -60,8 +58,8 @@ class LegislationSpider(scrapy.Spider):
                     continue
 
             else:
-            	if not href.endswith(".xml"):
-            		self.logger.info("class %s found is not file or directory for tag %s" %(li_class, li.extract()))
+            	if not li_class == "file":
+            		self.logger.info("class %s found is not .xml file or directory for tag %s" %(li_class, li.extract()))
 
 
 
